ee9e70f4458e063f94b583c5a098a6494b3940f1
Aishsara <113768211+Aishsara@users.noreply.github.com>
2023-06-21 21:13:06 +0530

    html little game

--- index.js
+++ index.js
@@ -1,38 +1,12 @@
-const dino =document.getElementById("dino");
-const cactus = document.getElementById("cactus");
-let a=1;
-
-function jump()
+function game()
 {
-    if(dino.classList !="jump" ){
-        dino.classList.add("jump"); 
-        
-        a=a+1;
-        document.getElementById("score").innerHTML=a;
-        setTimeout(function(){dino.classList.remove("jump");},300 ); 
-    }
-} 
-
-let isAlive = setInterval(function () {
-    //get current dino Y position
-    let dinoTop =parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
-    
-    //get current dino X position
-    let cactusLeft=parseInt(window.getComputedStyle(cactus).getPropertyValue("left")  );
-    
-    
-    //detect collision
-    if(cactusLeft<50 && cactusLeft>0 && dinoTop>=150   )
+    const val=document.getElementById("guess").value;
+    const rand="Hi";
+    if(val==rand)
     {
-        //collision
-        document.getElementById("max1").innerHTML=a;   
-        a=0;
-        alert("Game Over!")
+        alert("Had luck You Win dude!!");
     }
-
-
-},10  );
-document.addEventListener("keydown",function(event)
-{
-    jump();
-});   
+    else{
+        alert("You suck ,you loser!!!");
+    }
+}
